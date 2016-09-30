d7168c51367ba1afd94f3f2ae204f580c877d552
queensoft20 <queensoft20@gmail.com>
2016-09-30 21:02:44 +0330

    Add files via upload

---
+++ P_01.java
@@ -0,0 +1,25 @@
+package euler;
+
+/**
+ * Created by pari on 9/30/2016.
+ */
+public class P_01 {
+    public static void main(String[] args) {
+
+        sumOfMultiples();
+    }
+    public static void sumOfMultiples()
+    {
+        int n=1;
+        int sum=0;
+        while (n<1000){
+            if (n%3==0 || n%5==0){
+                sum=sum+n;
+
+            }
+            n++;
+            System.out.println(sum);
+        }
+    }
+
+}
